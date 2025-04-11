f79e12a463f5b1758c91a3c10397432825771767
ed <ed.070205wilson@gmail.com>
2025-04-11 01:07:08 -0500

    Primer commit del sitio con navbar corregido

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,87 +1,70 @@
 (function ($) {
-	
 	"use strict";
-
-	// Page loading animation
-	$(window).on('load', function() {
-
-        $('#js-preloader').addClass('loaded');
-
-    });
-
-	// WOW JS
-	$(window).on ('load', function (){
-        if ($(".wow").length) { 
-            var wow = new WOW ({
-                boxClass:     'wow',      // Animated element css class (default is wow)
-                animateClass: 'animated', // Animation css class (default is animated)
-                offset:       20,         // Distance to the element when triggering the animation (default is 0)
-                mobile:       true,       // Trigger animations on mobile devices (default is true)
-                live:         true,       // Act on asynchronously loaded content (default is true)
-            });
-            wow.init();
-        }
-    });
-
-	$(window).scroll(function() {
-	  var scroll = $(window).scrollTop();
-	  var box = $('.header-text').height();
-	  var header = $('header').height();
-
-	  if (scroll >= box - header) {
-	    $("header").addClass("background-header");
-	  } else {
-	    $("header").removeClass("background-header");
+  
+	// =================== PAGE LOADING ===================
+	$(window).on('load', function () {
+	  $('#js-preloader').addClass('loaded');
+	});
+  
+	// =================== WOW ANIMATIONS ===================
+	$(window).on('load', function () {
+	  if ($(".wow").length) {
+		var wow = new WOW({
+		  boxClass: 'wow',
+		  animateClass: 'animated',
+		  offset: 20,
+		  mobile: true,
+		  live: true,
+		});
+		wow.init();
 	  }
 	});
-	
-	$('.filters ul li').click(function(){
-        $('.filters ul li').removeClass('active');
-        $(this).addClass('active');
-          
-          var data = $(this).attr('data-filter');
-          $grid.isotope({
-            filter: data
-          })
-        });
-
-        var $grid = $(".grid").isotope({
-          	itemSelector: ".all",
-          	percentPosition: true,
-          	masonry: {
-            columnWidth: ".all"
-        }
-    })
-
-	var width = $(window).width();
-		$(window).resize(function() {
-			if (width > 992 && $(window).width() < 992) {
-				location.reload();
-			}
-			else if (width < 992 && $(window).width() > 992) {
-				location.reload();
-			}
-	})
-
-
-
-	$(document).on("click", ".naccs .menu div", function() {
-		var numberIndex = $(this).index();
-	
-		if (!$(this).is("active")) {
-			$(".naccs .menu div").removeClass("active");
-			$(".naccs ul li").removeClass("active");
-	
-			$(this).addClass("active");
-			$(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
-	
-			var listItemHeight = $(".naccs ul")
-				.find("li:eq(" + numberIndex + ")")
-				.innerHeight();
-			$(".naccs ul").height(listItemHeight + "px");
-		}
+  
+	// =================== FILTROS ===================
+	$('.filters ul li').click(function () {
+	  $('.filters ul li').removeClass('active');
+	  $(this).addClass('active');
+  
+	  var data = $(this).attr('data-filter');
+	  $grid.isotope({
+		filter: data
+	  });
 	});
-
+  
+	var $grid = $(".grid").isotope({
+	  itemSelector: ".all",
+	  percentPosition: true,
+	  masonry: {
+		columnWidth: ".all"
+	  }
+	});
+  
+	// =================== RECARGAR AL CAMBIAR WIDTH ===================
+	var width = $(window).width();
+	$(window).resize(function () {
+	  if ((width > 992 && $(window).width() < 992) ||
+		  (width < 992 && $(window).width() > 992)) {
+		location.reload();
+	  }
+	});
+  
+	// =================== TABS INTERACTIVOS ===================
+	$(document).on("click", ".naccs .menu div", function () {
+	  var numberIndex = $(this).index();
+  
+	  if (!$(this).is("active")) {
+		$(".naccs .menu div").removeClass("active");
+		$(".naccs ul li").removeClass("active");
+  
+		$(this).addClass("active");
+		$(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+  
+		var listItemHeight = $(".naccs ul")
+		  .find("li:eq(" + numberIndex + ")")
+		  .innerHeight();
+		$(".naccs ul").height(listItemHeight + "px");
+	  }
+	});
 	$('.owl-features').owlCarousel({
 		items:3,
 		loop:true,
